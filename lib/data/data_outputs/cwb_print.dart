ce50a103425d157c6809647a62b2667f6ce784f4
4amshin <shinmendenz@gmail.com>
2024-10-20 14:31:32 +0800

    Implement OrderPage Code

---
+++ lib/data/data_outputs/cwb_print.dart
@@ -0,0 +1,125 @@
+import 'package:esc_pos_utils_plus/esc_pos_utils_plus.dart';
+import 'package:fic12_fe/core/extensions/int_ext.dart';
+import 'package:fic12_fe/core/extensions/string_ext.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+import 'package:image/image.dart' as img;
+
+import '../../presentation/home/models/order_item.dart';
+
+class CwbPrint {
+  CwbPrint._init();
+
+  static final CwbPrint instance = CwbPrint._init();
+
+  Future<List<int>> printOrder(
+    List<OrderItem> products,
+    int totalQuantity,
+    int totalPrice,
+    String paymentMethod,
+    int nominalBayar,
+    String namaKasir,
+  ) async {
+    List<int> bytes = [];
+
+    final profile = await CapabilityProfile.load();
+    final generator = Generator(PaperSize.mm58, profile);
+
+    final ByteData data = await rootBundle.load('assets/logo/mylogo.png');
+    final Uint8List bytesData = data.buffer.asUint8List();
+    final img.Image? orginalImage = img.decodeImage(bytesData);
+    bytes += generator.reset();
+
+    if (orginalImage != null) {
+      final img.Image grayscalledImage = img.grayscale(orginalImage);
+      final img.Image resizedImage =
+          img.copyResize(grayscalledImage, width: 240);
+      bytes += generator.imageRaster(resizedImage, align: PosAlign.center);
+      bytes += generator.feed(2);
+    }
+
+    bytes += generator.text('Coffee With Bahri',
+        styles: const PosStyles(
+          bold: true,
+          align: PosAlign.center,
+          height: PosTextSize.size1,
+          width: PosTextSize.size1,
+        ));
+
+    bytes += generator.text('Flutter Street, Dart 2024',
+        styles: const PosStyles(bold: false, align: PosAlign.center));
+    bytes += generator.text(
+        'Date : ${DateFormat('dd-MM-yyyy HH:mm').format(DateTime.now())}',
+        styles: const PosStyles(bold: false, align: PosAlign.center));
+
+    bytes += generator.feed(1);
+    bytes += generator.text('Order Items:',
+        styles: const PosStyles(bold: false, align: PosAlign.center));
+
+    for (final product in products) {
+      bytes += generator.text(product.product.name,
+          styles: const PosStyles(align: PosAlign.left));
+
+      bytes += generator.row([
+        PosColumn(
+          text: '${product.product.price} x ${product.quantity}',
+          width: 8,
+          styles: const PosStyles(align: PosAlign.left),
+        ),
+        PosColumn(
+          text: '${product.product.price * product.quantity}'.currencyFormatRp,
+          width: 4,
+          styles: const PosStyles(align: PosAlign.right),
+        ),
+      ]);
+    }
+
+    bytes += generator.feed(1);
+
+    bytes += generator.row([
+      PosColumn(
+        text: 'Total',
+        width: 6,
+        styles: const PosStyles(align: PosAlign.left),
+      ),
+      PosColumn(
+        text: totalPrice.currencyFormatRp,
+        width: 6,
+        styles: const PosStyles(align: PosAlign.right),
+      ),
+    ]);
+
+    bytes += generator.row([
+      PosColumn(
+        text: 'Pay',
+        width: 6,
+        styles: const PosStyles(align: PosAlign.left),
+      ),
+      PosColumn(
+        text: nominalBayar.currencyFormatRp,
+        width: 6,
+        styles: const PosStyles(align: PosAlign.right),
+      ),
+    ]);
+
+    bytes += generator.row([
+      PosColumn(
+        text: 'Payment Method',
+        width: 8,
+        styles: const PosStyles(align: PosAlign.left),
+      ),
+      PosColumn(
+        text: paymentMethod == 'QRIS' ? 'QRIS' : 'Cash',
+        width: 4,
+        styles: const PosStyles(align: PosAlign.right),
+      ),
+    ]);
+
+    bytes += generator.feed(1);
+    bytes += generator.text('Thank you for coming',
+        styles: const PosStyles(bold: false, align: PosAlign.center));
+    bytes += generator.feed(3);
+
+    return bytes;
+  }
+}
--- lib/presentation/order/pages/order_page.dart
+++ lib/presentation/order/pages/order_page.dart
@@ -1,4 +1,19 @@
+import 'package:fic12_fe/core/assets/assets.gen.dart';
+import 'package:fic12_fe/core/components/menu_button.dart';
+import 'package:fic12_fe/core/components/spaces.dart';
+import 'package:fic12_fe/core/constants/colors.dart';
+import 'package:fic12_fe/core/extensions/build_context_ext.dart';
+import 'package:fic12_fe/presentation/home/bloc/checkout/checkout_bloc.dart';
+import 'package:fic12_fe/presentation/home/models/order_item.dart';
+import 'package:fic12_fe/presentation/home/pages/dashboard_page.dart';
+import 'package:fic12_fe/presentation/order/bloc/order/order_bloc.dart';
+import 'package:fic12_fe/presentation/order/widgets/order_card.dart';
+import 'package:fic12_fe/presentation/order/widgets/payment_cash_dialog.dart';
+import 'package:fic12_fe/presentation/order/widgets/payment_qris_dialog.dart';
+import 'package:fic12_fe/presentation/order/widgets/process_button.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class OrderPage extends StatefulWidget {
   const OrderPage({super.key});
@@ -8,8 +23,199 @@ class OrderPage extends StatefulWidget {
 }
 
 class _OrderPageState extends State<OrderPage> {
+  final indexValue = ValueNotifier(0);
+
+  List<OrderItem> orders = [];
+
+  int totalPrice = 0;
+  int calculateTotalPrice(List<OrderItem> orders) {
+    return orders.fold(
+        0,
+        (previousValue, element) =>
+            previousValue + element.product.price * element.quantity);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const paddingHorizontal = EdgeInsets.symmetric(horizontal: 16.0);
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            context.push(const DashboardPage());
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        title: const Text(
+          'Order Detail',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              //show dialog save order
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text('Open Bill'),
+                      content: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Order Name',
+                        ),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            //clear checkout
+                            context.read<CheckoutBloc>().add(
+                                  const CheckoutEvent.started(),
+                                );
+                            //open bill success snack bar
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Open Bill Success'),
+                                backgroundColor: AppColors.primary,
+                              ),
+                            );
+
+                            context.pushReplacement(const DashboardPage());
+                          },
+                          child: const Text('Save'),
+                        ),
+                      ],
+                    );
+                  });
+            },
+            icon: const Icon(Icons.save_as_outlined),
+          ),
+          const SpaceWidth(8),
+        ],
+      ),
+      body: BlocBuilder<CheckoutBloc, CheckoutState>(
+        builder: (context, state) {
+          return state.maybeWhen(orElse: () {
+            return const Center(
+              child: Text('No Data'),
+            );
+          }, success: (data, qty, total) {
+            if (data.isEmpty) {
+              return const Center(
+                child: Text('No Data'),
+              );
+            }
+
+            totalPrice = total;
+            return ListView.separated(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              itemCount: data.length,
+              separatorBuilder: (context, index) => const SpaceHeight(20.0),
+              itemBuilder: (context, index) => OrderCard(
+                padding: paddingHorizontal,
+                data: data[index],
+                onDeleteTap: () {},
+              ),
+            );
+          });
+        },
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 16, 16, 28),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            BlocBuilder<CheckoutBloc, CheckoutState>(
+              builder: (context, state) {
+                return state.maybeWhen(
+                  orElse: () {
+                    return const SizedBox.shrink();
+                  },
+                  success: (data, qty, total) {
+                    return ValueListenableBuilder(
+                      valueListenable: indexValue,
+                      builder: (context, value, _) => Row(
+                        children: [
+                          MenuButton(
+                            iconPath: Assets.icons.cash.path,
+                            label: 'CASH',
+                            isActive: value == 1,
+                            onPressed: () {
+                              indexValue.value = 1;
+                              context
+                                  .read<OrderBloc>()
+                                  .add(OrderEvent.addPaymentMethod(
+                                    paymentMethod: 'Tunai',
+                                    orders: data,
+                                  ));
+                            },
+                          ),
+                          const SpaceWidth(16.0),
+                          MenuButton(
+                            iconPath: Assets.icons.qrCode.path,
+                            label: 'QR',
+                            isActive: value == 2,
+                            onPressed: () {
+                              indexValue.value = 2;
+                              context
+                                  .read<OrderBloc>()
+                                  .add(OrderEvent.addPaymentMethod(
+                                    paymentMethod: 'QRIS',
+                                    orders: data,
+                                  ));
+                            },
+                          ),
+                          const SpaceWidth(16.0),
+                          MenuButton(
+                            iconPath: Assets.icons.debit.path,
+                            label: 'TRANSFER',
+                            isActive: value == 3,
+                            onPressed: () {
+                              indexValue.value = 3;
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+            const SpaceHeight(20.0),
+            ProcessButton(
+              price: 0,
+              onPressed: () async {
+                if (indexValue.value == 0) {
+                } else if (indexValue.value == 1) {
+                  showDialog(
+                    context: context,
+                    builder: (context) => PaymentCashDialog(
+                      price: totalPrice,
+                    ),
+                  );
+                } else if (indexValue.value == 2) {
+                  showDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (context) => PaymentQrisDialog(
+                      price: totalPrice,
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
